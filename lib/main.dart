6c92d4b4d9097aeda3a2b2f415cc39fa681fbda8
Noel Pinto <noelpinto47@gmail.com>
2023-10-04 02:00:13 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:ecostore/Views/Home/homepage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -13,6 +16,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+
     return Sizer(builder: (context, orientation, deviceType) {
       return GetMaterialApp(
         home: HomePage(),
